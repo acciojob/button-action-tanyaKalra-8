9204cae0efb8f4c2c125ea1ad6cafe228efc8e7f
Taniya <tanya.kalra200@gmail.com>
2023-07-17 21:21:26 +0530

    submitting code-Mon Jul 17 2023 21:21:21 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import './../styles/App.css';
 
 const App = (props) => {
 
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleVisible(){
+    setIsVisible(true);
+  }
+
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={handleVisible}>Click Me</button>
+      <p id="para" className={isVisible? "show": "hide"}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>
     </div>
   );
 }
